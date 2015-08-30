7996665bb83fffccaff9d54a8fe77ff12c4ef591
Sam Bender <rednebmas@gmail.com>
2015-08-29 19:24:13 -0700

    Initial Commit

---
+++ TouchBubble/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchBubble
+//
+//  Created by Sam Bender on 8/29/15.
+//  Copyright (c) 2015 Sam Bender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TouchBubble/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TouchBubble
+//
+//  Created by Sam Bender on 8/29/15.
+//  Copyright (c) 2015 Sam Bender. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
